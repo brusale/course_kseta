fa4674f22a66641378d1dd73707a922148929ebd
augsteinboson <augustin.vestner@kit.edu>
2024-10-18 14:54:02 +0200

    Data format change\n\nBreaks old code.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-data = np.loadtxt("data.csv")
+data = np.loadtxt("data.csv", delimiter=',')
 plt.plot(np.arange(20), np.poly1d(np.polyfit(data[:,0], data[:,1], 1))(np.arange(20)), color='r')
 plt.scatter(data[:,0], data[:,1])
 plt.xlabel('x')
