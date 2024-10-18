4a4c65499c1dd11db1132772352e19708cd586e9
Alessandro Brusamolino <brusale11@gmail.com>
2024-10-18 16:27:51 +0200

    improve poly fit test
    Added assert_almost_equal to check if the fitting function returns
    reasonable values.

--- test.py
+++ test.py
@@ -1,14 +1,25 @@
 import unittest
 from src.Fitter import Fitter
+from unittest.mock import patch
+from numpy.testing import assert_almost_equal
 import numpy as np
 
 class Test(unittest.TestCase):
+	
+	def setUp(self):
+		self.x_ = np.linspace(0,10, 1000)
+		self.y_ = self.x_
 
 	def test_fit(self):
 		x = np.linspace(0,10, 1000)
 		y = x
 		fitter = Fitter(x,y)
-		fitter.fit()
+		m, q = fitter.fit()
+		regr_y = m * self.x_ + q
+		assert_almost_equal(regr_y, y)
 
+	def tearDown(self):
+ 		patch.stopall()
+	
 if __name__ == "__main__":
 	unittest.main()
